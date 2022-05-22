d704adcf60bfc5fc90bc5687e80b7ba72ad413fa
Andrey Lebedev <ILAndVik@yandex.ru>
2022-05-22 21:20:48 +0500

    HW2 is completed

---
+++ hw02_unpack_string/unpack/unpack.go
@@ -0,0 +1,55 @@
+package unpack
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var ErrInvalidString = errors.New("неверный формат строки")
+
+func Unpack(str string) (string, error) {
+	var stringBuffer string
+	var flag bool
+	var resultString strings.Builder
+
+	for _, symbol := range str {
+		if unicode.IsDigit(symbol) {
+			if stringBuffer == "" {
+				return "", ErrInvalidString
+			}
+
+			if stringBuffer != "" && !flag {
+				count, _ := strconv.Atoi(string(symbol))
+				// Дублируем символ количеством - count и записываем в буфер
+				resultString.WriteString(strings.Repeat(stringBuffer, count))
+				stringBuffer = ""
+				continue
+			}
+		}
+
+		if stringBuffer != "" {
+			if isSymbolEscape(symbol) && !flag {
+				flag = true
+				continue
+			}
+
+			resultString.WriteString(stringBuffer)
+			stringBuffer = string(symbol)
+			flag = false
+			continue
+
+		}
+		stringBuffer = string(symbol)
+	}
+	resultString.WriteString(stringBuffer)
+
+	// Методом String() - окончательно собираем строку из буфера
+	return resultString.String(), nil
+}
+
+// Проверка. Символ это пробел
+func isSymbolEscape(r rune) bool {
+	return r == 92
+}
---
+++ hw02_unpack_string/unpack/unpack_test.go
@@ -0,0 +1,90 @@
+package unpack
+
+import (
+	"errors"
+	"testing"
+	"unicode"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "abccd", expected: "abccd"},
+		{input: "", expected: ""},
+		{input: "aaa0b", expected: "aab"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `Ä4Ñ2`, expected: `ÄÄÄÄÑÑ`},
+		{input: `퉄3팿3`, expected: `퉄퉄퉄팿팿팿`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestUnpackInvalidString(t *testing.T) {
+	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+func TestStringOnlyDigit(t *testing.T) {
+	input := "78784564"
+	if result, err := Unpack(input); err == nil {
+		t.Errorf("Строка содержит толькко цифры \"%s\", expect error", result)
+	}
+}
+func TestWrongSymbolSequence(t *testing.T) {
+	input := "a2b37"
+	if result, err := Unpack(input); err == nil {
+		t.Errorf("Неверная последовательность символов \"%s\", expect error", result)
+	}
+}
+func TestIsSymbolDigit(t *testing.T) {
+	digitsRunes := []rune("0123456789")
+	for _, digitRune := range digitsRunes {
+		if !unicode.IsDigit(digitRune) {
+			t.Errorf("Символ \"%s\" не цифра", string(digitRune))
+		}
+	}
+
+	invalidDigitsRunes := []rune("abcXYZ!@#")
+	for _, digitRune := range invalidDigitsRunes {
+		if unicode.IsDigit(digitRune) {
+			t.Errorf("Символ \"%s\" цифра", string(digitRune))
+		}
+	}
+}
+
+func TestIsSymbolEscape(t *testing.T) {
+	validEscapeRunes := []rune(`\`)
+	for _, escapeRune := range validEscapeRunes {
+		if !isSymbolEscape(escapeRune) {
+			t.Errorf("Символ \"%s\" не пробел", string(escapeRune))
+		}
+	}
+
+	invalidEscapeRunes := []rune(`|/#@!`)
+	for _, escapeRune := range invalidEscapeRunes {
+		if isSymbolEscape(escapeRune) {
+			t.Errorf("Символ \"%s\" пробел", string(escapeRune))
+		}
+	}
+}
